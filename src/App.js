bc720e3a11e3ddbbb39e85b0db2f13634bf76a16
Henrik Danielsson <henrik.danielsson@me.com>
2023-12-25 15:51:11 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,58 @@
+import React, { useState } from 'react';
+import { Logo } from '@hjdse/146north';
+import { langTxt } from './lang/language.js';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>14<span className="reverse">6</span><span className="small">.S<span className="rotate">e</span></span></h1>
-        <p>A placeholder for future projects.</p>
-        <img src='gfx/instagram-16.png' onClick={() => {
-      window.open("http://instagram.com/146_north", "_blank");
-    }} alt="Instagram"></img>
-      </header>
-    </div>
-  );
+	const [lang, setLang] = useState();
+	const [loading, setLoading] = useState();
+
+	const handleclick = (prop) => {
+		setLoading(true);
+		setLang(prop);
+		localStorage.setItem('lang', prop);
+		setTimeout(() => {
+			setLoading(false);
+		}, 1000);
+	};
+
+	return (
+		<div id="App" className="App">
+			<Logo
+				text="Layout | Code | Photography"
+				size={!lang ? '100%' : '60%'}
+				margin={!lang ? '24vh,5vh' : '10vh,5vh'}
+			/>
+			{loading && (
+				<main>
+					<p className="center">{langTxt[lang].load}</p>
+				</main>
+			)}
+			{!lang && (
+				<main>
+					<p>Please choose language:</p>
+					<p className="center">
+						<span
+							onClick={() => {
+								handleclick('eng');
+							}}
+						>
+							English
+						</span>{' '}
+						|{' '}
+						<span
+							onClick={() => {
+								handleclick('sve');
+							}}
+						>
+							Swedish
+						</span>
+					</p>
+				</main>
+			)}
+			{!loading && lang && langTxt[lang].text}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/lang/language.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+export const langTxt = {
+	sve: {
+		load: 'Vänligen vänta..',
+		text: (
+			<main>
+				<p>Hej</p>
+				<p>
+					Mitt namn är Henrik och jag gillar webutveckling, fotografering och
+					video redigering. Se denna sida som en "placeholder" för framtida
+					projekt.
+				</p>
+				<p>
+					Just nu gör jag en sida som ska det göra enklare för oss tekniker som
+					jobbar med Aptus och RCO att addressera dessa, sidan hittar ni på:
+				</p>
+				<p className="center">
+					<a href="https://binary.146.se">binary.146.se</a>
+				</p>
+				<p>
+					Vill man hellre se mina gamla bilder och filmklipp så finns dom här:
+				</p>
+				<p>
+					<a href="https://www.instagram.com/146_north">Instagram</a>,{' '}
+					<a href="https://www.500px.com/">500px</a> eller{' '}
+					<a href="https://youtube.com/c/HenrikDanielsson81">Youtube</a>.
+				</p>
+			</main>
+		),
+	},
+	eng: {
+		load: 'Please wait...',
+		text: (
+			<main>
+				<p>Hello</p>
+				<p>
+					My name is Henrik, I dabble a bit in webdev, photography and
+					videography. Consider this a placeholder for future projects.
+				</p>
+				<p>
+					Right now I'm working on a site that will make it easier for us
+					technicians working with Aptus and RCO to address these, you can find
+					the site at:
+				</p>
+				<p className="center">
+					<a href="https://binary.146.se">binary.146.se</a>
+				</p>
+				<p>
+					If you prefer to see my pictures and videos, they are available here:
+				</p>
+				<p>
+					<a href="https://www.instagram.com/146_north">Instagram</a>,{' '}
+					<a href="https://www.500px.com/">500px</a> or{' '}
+					<a href="https://youtube.com/c/HenrikDanielsson81">Youtube</a>.
+				</p>
+			</main>
+		),
+	},
+};
